4e7d5c5db1327315674c013bb29494d411f61148
arabelazulfa <arabelazulfalaila08@students.unnes.ac.id>
2025-07-01 23:15:31 +0700

    ubah2

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ feature_names = joblib.load("feature_names.pkl")
 st.title("Customer Churn Prediction")
 st.write("Masukkan data pelanggan di bawah ini untuk memprediksi kemungkinan churn.")
 
+# Input dari user
 gender = st.selectbox("Gender", ["Female", "Male"])
 senior = st.selectbox("Senior Citizen", [0, 1])
 partner = st.selectbox("Partner", ["Yes", "No"])
@@ -21,7 +22,7 @@ contract = st.selectbox("Contract Type", ["Month-to-month", "One year", "Two yea
 monthly_charges = st.number_input("Monthly Charges", 0.0, 200.0, 70.0)
 total_charges = st.number_input("Total Charges", 0.0, 10000.0, 3000.0)
 
-# Buat input dict
+
 input_dict = {
     "gender": gender,
     "SeniorCitizen": senior,
@@ -35,12 +36,25 @@ input_dict = {
     "TotalCharges": total_charges,
 }
 
-# Konversi jadi DataFrame
+# Konversi ke DataFrame
 input_data = pd.DataFrame([input_dict])
 
-# Pastikan kolom urut dan lengkap
+
+input_data["gender"] = input_data["gender"].map({"Female": 0, "Male": 1})
+input_data["Partner"] = input_data["Partner"].map({"Yes": 1, "No": 0})
+input_data["Dependents"] = input_data["Dependents"].map({"Yes": 1, "No": 0})
+input_data["PhoneService"] = input_data["PhoneService"].map({"Yes": 1, "No": 0})
+input_data["InternetService"] = input_data["InternetService"].map({
+    "DSL": 0, "Fiber optic": 1, "No": 2
+})
+input_data["Contract"] = input_data["Contract"].map({
+    "Month-to-month": 0, "One year": 1, "Two year": 2
+})
+
+
 input_data = input_data.reindex(columns=feature_names)
 
+# Prediksi
 if st.button("Prediksi Churn"):
     prob = model.predict_proba(input_data)[0][1]
     pred = int(prob >= threshold)
